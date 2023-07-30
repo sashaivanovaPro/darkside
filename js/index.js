5614210c8e26c8d084c5bed8bf93a85a345cda9e
Sasha Ivanova <sashaivanova.pro@gmail.com>
2023-07-30 23:56:09 +0600

    fix:stylesheet linking

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("mouseover", (e) => {
   Object.assign(document.documentElement, {
     style: `
     --move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-    --move-y: ${(e.clientY - window.innerHeight / 2) * -0.008}deg ;
+    --move-y: ${(e.clientY - window.innerHeight / 2) * -0.01}deg ;
     `,
   });
 });
